f82ca8e07e91d27658e739d8698bed0c3cc882b9
Joshua Wise <joshua@joshuawise.com>
2025-02-21 01:09:32 -0500

    bluetooth/nrf52: use nordic Device Information Service in bt_driver_start

--- src/bluetooth-fw/nrf52/init.c
+++ src/bluetooth-fw/nrf52/init.c
@@ -1,5 +1,6 @@
 #include "nrf_sdh.h"
 #include "nrf_sdh_ble.h"
+#include "ble_dis.h"
 #include "ble_advdata.h"
 
 #include "comm/bt_lock.h"
@@ -12,6 +13,8 @@
 #include "board/board.h"
 
 #include <bluetooth/init.h>
+#include <bluetooth/dis.h>
+#include <bluetooth/id.h>
 #include <bluetooth/qemu_transport.h>
 
 #include <stdlib.h>
@@ -49,18 +52,18 @@ void bt_driver_init(void) {
   ret_code_t rv;
 
   bt_lock_init();
-  
+
   /* the softdevice was enabled by early init so the LFCLK / RTC will work */
   PBL_LOG(LOG_LEVEL_INFO, "nRF52: enabling BLE");
-  
+
   uint32_t ram_start = 0;
   extern uint8_t __KERNEL_RAM_start__;
   uint32_t pebbleos_ram_start = (uint32_t) &__KERNEL_RAM_start__;
-  
+
   rv = nrf_sdh_ble_default_cfg_set(CONN_TAG, &ram_start);
   PBL_ASSERTN(rv == NRF_SUCCESS);
   PBL_ASSERTN(ram_start <= pebbleos_ram_start);
-  
+
   rv = nrf_sdh_ble_enable(&pebbleos_ram_start);
   PBL_ASSERTN(rv == NRF_SUCCESS);
   PBL_LOG(LOG_LEVEL_INFO, "nRF52: BLE stack enabled");
@@ -74,8 +77,36 @@ static ble_gap_adv_data_t _advdata = {
   .scan_rsp_data = { .p_data = _srdata_buf, .len = sizeof(_srdata_buf) },
 };
 
+static DisInfo s_dis_info;
+
 bool bt_driver_start(BTDriverConfig *config) {
-  PBL_LOG(LOG_LEVEL_INFO, "nRF52: BLE dummy driver start");
+  PBL_LOG(LOG_LEVEL_INFO, "nRF52: BLE driver start");
+
+  ret_code_t rv;
+
+  /* XXX: config->root_keys: sd_ble_gap_device_identities_set -- https://docs.nordicsemi.com/bundle/s140_v7.2.0_api/page/group_b_l_e_g_a_p_f_u_n_c_t_i_o_n_s.html#ga16fa7a0ac608c4955a2e4feea7a10784 */
+
+  /* BLE device information service */
+  s_dis_info = config->dis_info;
+  static ble_dis_init_t dis_init = {
+    .dis_char_rd_sec = SEC_OPEN /* XXX: or SEC_JUST_WORKS? */,
+    .model_num_str = { .length = sizeof(s_dis_info.model_number), .p_str = (uint8_t *) &s_dis_info.model_number },
+    .manufact_name_str = { .length = sizeof(s_dis_info.manufacturer), .p_str = (uint8_t *) &s_dis_info.manufacturer },
+    .serial_num_str = { .length = sizeof(s_dis_info.serial_number), .p_str = (uint8_t *) &s_dis_info.serial_number },
+    .fw_rev_str = { .length = sizeof(s_dis_info.fw_revision), .p_str = (uint8_t *) &s_dis_info.fw_revision },
+    .sw_rev_str = { .length = sizeof(s_dis_info.sw_revision), .p_str = (uint8_t *) &s_dis_info.sw_revision },
+  };
+  rv = ble_dis_init(&dis_init);
+  PBL_ASSERTN(rv == NRF_SUCCESS);
+
+  /* config->identity_addr is an OUTPUT from here, but TI does not set it.  all the same, we will do it here */
+  bt_driver_id_copy_local_identity_address(&config->identity_addr);
+
+  /* config->is_hrm_supported_and_enabled */
+
+  /* XXX: later, these should go into advertising init */
+
+  PBL_ASSERTN(!config->is_hrm_supported_and_enabled);
   static uint8_t _name_buf[24] = "Asterix softdevice";
   
   ble_gap_conn_sec_mode_t sec_mode;
@@ -86,7 +117,6 @@ bool bt_driver_start(BTDriverConfig *config) {
   
   ble_advdata_t advdata;
   ble_advdata_t srdata;
-  ret_code_t rv;
   
   ble_gap_adv_params_t advparams;
   
@@ -122,8 +152,12 @@ bool bt_driver_start(BTDriverConfig *config) {
 }
 
 void bt_driver_stop(void) {
+  /* XXX: all to do here is shut down advertising, and sd_ble_gap_disconnect
+   * if we are connected; this will shut down the radio entirely (and there
+   * is no other way to reset the softdevice through airplane mode or any
+   * such, if it wedges you're on your own and it's time to boot) */
 }
 
 void bt_driver_power_down_controller_on_boot(void) {
-  // no-op
+  // nRF BLE boots up powered off (it's internal), so nothign to do here
 }
