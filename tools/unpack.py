7e020e36bd98cc91873eb01715f3eced8c673939
Stasia Michalska <hel@lcp.world>
2025-11-05 22:06:49 +0100

    tools/unpack.py: Output the files from pbpack in the right order and into a directory
    Signed-off-by: Stasia Michalska <hel@lcp.world>

--- tools/unpack.py
+++ tools/unpack.py
@@ -15,6 +15,7 @@
 import os
 import sys
 import argparse
+import pathlib
 
 from pbpack import ResourcePack
 
@@ -23,16 +24,19 @@ def main():
         'Unpack pbpacked data to recover original file content.')
     
     parser.add_argument('pbpack', type=str, help='app_resources.pbpack file to unpack')
+    parser.add_argument('--output', default='.', help='the directory to output the files to')
     parser.add_argument('--app', default=False, action='store_true',
                         help='Indicate this pbpack is an app pbpack')
-    
+
     args = parser.parse_args()
 
     if os.path.exists(args.pbpack):
-        resource_pack = ResourcePack().deserialize(open(args.pbpack,'rb'), is_system=not args.app)
-        for idx, resource_data in enumerate(resource_pack.contents):
-            with open(str(idx) + '.dat','wb') as outfile: 
-                outfile.write(resource_data)
+        pathlib.Path(args.output).mkdir(parents=True, exist_ok=True)
+
+        resource_pack = ResourcePack.deserialize(open(args.pbpack,'rb'), is_system=not args.app)
+        for idx, resource_data in enumerate(resource_pack.table_entries):
+            with open(os.path.join(args.output, str(idx) + '.dat'),'wb') as outfile:
+                outfile.write(resource_pack.contents[resource_data.content_index])
 
 
 if __name__ == '__main__':
